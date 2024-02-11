eab14dbb22c803b48eaa6a5e215cee7f54c49947
Emelyanov <westbrookokc1997@gmail.com>
2024-02-11 12:02:57 +0300

    init

---
+++ src/main/java/ru/seedel/pastetbox/PastetboxApplication.java
@@ -0,0 +1,13 @@
+package ru.seedel.pastetbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastetboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastetboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/seedel/pastetbox/PastetboxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.seedel.pastetbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastetboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
